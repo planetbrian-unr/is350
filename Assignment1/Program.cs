a3b31bdc2ee33e538b37d1710567742d941d3186
Brian Wu <brianwu@unr.edu>
2025-01-22 23:22:10 +0000

    "complete" assignment 1. need to refactor to dr. yang's preferred coding style

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+    Brian Wu
+    IS 350: Assignment 1
+    C# console application stating my goals this semester
+*/
+Console.WriteLine("Hello world");
+Console.WriteLine("Short-term goal: I want to stay ahead in this course as I feel confident in the material that will be learned.");
+Console.WriteLine("Mid-point goal: I want to understand C# well enough to write simple programs.");
+Console.WriteLine("End-of-semester goal: I want to use C# to reimplement some of my own existing projects.");
+
+
