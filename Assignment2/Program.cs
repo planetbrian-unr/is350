c13e144c441170ff3553e9cdb49780d19dbfeaa5
Brian Wu <brianwu@unr.edu>
2025-01-29 22:53:46 +0000

    rewrote to reflect in-class examples' syntax

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -23,10 +23,11 @@ double rectangleArea = rectangleLength * rectangleWidth;
 double rectanglePerimeter = (2 * rectangleLength) + (2 * rectangleWidth);
 int squareArea = squareLength * squareLength;
 int squarePerimeter = 4 * squareLength;
+double areaSum = rectangleArea + squareArea;
 
-Console.WriteLine("Hello " + name);
-Console.WriteLine("Rectangle area: " + rectangleArea);
-Console.WriteLine("Rectangle perimeter: " + rectanglePerimeter);
-Console.WriteLine("Square area: " + squareArea);
-Console.WriteLine("Square perimeter: " + squarePerimeter);
-Console.WriteLine("Sum: " + (rectangleArea + squareArea));
+Console.WriteLine("Hello {0}", name);
+Console.WriteLine("Rectangle area: {0}", rectangleArea);
+Console.WriteLine("Rectangle perimeter: {0}", rectanglePerimeter);
+Console.WriteLine("Square area: {0}", squareArea);
+Console.WriteLine("Square perimeter: {0}", squarePerimeter);
+Console.WriteLine("Sum: {0}", areaSum);
