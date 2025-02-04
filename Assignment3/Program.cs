4f6c7d995b6bda0339f4a88fc6f8a37d4ea3211f
Brian Wu <brianwu@unr.edu>
2025-02-04 08:48:15 +0000

    finished assignment 3

---
+++ Assignment3/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+    Brian Wu
+    IS 350: Assignment 3
+    Writer or actor? Practicing if/switch statements and bools
+*/
+
+// declaring the writers/actors used later in this program
+string writer1 = "George Orwell", writer2 = "John Steinbeck";
+string actor1 = "Robert Downey Jr.", actor2 = "Scarlett Johansson";
+
+// for user input, either 1 or 2
+int input;
+
+Console.WriteLine("Is {0} is a writer (1) or an actor (2)?", writer1);  // asking the question, using a variable for the person's name
+input = Convert.ToInt32(Console.ReadLine());    // getting user input to be used in the if/else statement
+
+if(input == 1) {
+    Console.WriteLine("Good job! {0} is a writer!", writer1);
+}
+else {
+    Console.WriteLine("Incorrect! {0} is a writer!", writer1);
+}
+
+Console.WriteLine("Press enter to continue");
+Console.ReadKey();
+
+
+Console.WriteLine("Is {0} is a writer (1) or an actor (2)?", writer2);  // repeats for the others, changing if/else condition if needed
+input = Convert.ToInt32(Console.ReadLine());
+
+if(input == 1) {
+    Console.WriteLine("Good job! {0} is a writer!", writer2);
+}
+else {
+    Console.WriteLine("Incorrect! {0} is a writer!", writer2);
+}
+
+Console.WriteLine("Press enter to continue");
+Console.ReadKey();
+
+
+Console.WriteLine("Is {0} is a writer (1) or an actor (2)?", actor1);
+input = Convert.ToInt32(Console.ReadLine());
+
+if(input == 2) {
+    Console.WriteLine("Good job! {0} is an actor!", actor1);
+}
+else {
+    Console.WriteLine("Incorrect! {0} is an actor!", actor1);
+}
+
+Console.WriteLine("Press enter to continue");
+Console.ReadKey();
+
+
+Console.WriteLine("Is {0} is a writer (1) or an actor (2)?", actor2);
+input = Convert.ToInt32(Console.ReadLine());
+
+if(input == 2) {
+    Console.WriteLine("Good job! {0} is an actor!", actor2);
+}
+else {
+    Console.WriteLine("Incorrect! {0} is an actor!", actor2);
+}
+
+Console.WriteLine("Press enter to continue");
+Console.ReadKey();
