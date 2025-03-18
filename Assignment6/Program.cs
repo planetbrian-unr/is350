e1f09a8c1f27e81e7e1f9a3454559f5d68b4e8cf
Brian Wu <brianwu@unr.edu>
2025-03-18 22:33:55 +0000

    changes to a6

--- Assignment6/Program.cs
+++ Assignment6/Program.cs
@@ -7,24 +7,25 @@
 
 /* methods */
 // c. Draw, which returns a random number between 1 and 13
-static int draw() {
-    Random r = new Random();
+static int draw(Random r) {
     return r.Next(1, 14);
 }
 
 // d. War, no returned data, but takes two string parameters
 static void war(string player1Name, string player2Name) {
     int rounds = 0;
+    Random cardValue = new Random();
     int player1Cards = 26, player2Cards = 26;   // dii
-    string roundWinner;
 
     Console.WriteLine("\n");
 
     // diii. while loop to play War until either player has 0 cards
     while(player1Cards > 0 && player2Cards > 0) {
+        string roundWinner;
+        
         rounds++;
-        int player1Card = draw(), player2Card = draw(); // players draw cards
-
+        int player1Card = draw(cardValue), player2Card = draw(cardValue); // players draw cards
+ 
         // outputs values before comparing them to see who won the round.
         Console.WriteLine("Round {0}. {1}'s card: {2}. {3}'s card: {4}.", rounds, player1Name, player1Card, player2Name, player2Card);
         if(player1Card > player2Card) {
@@ -37,9 +38,10 @@ static void war(string player1Name, string player2Name) {
             player2Cards++;
             roundWinner = player2Name + " wins.";
         }
-        else
+        else {
             roundWinner = "Tie.";
-
+        }
+        
         // outputs round results
         Console.WriteLine("{0} {1} has {2} cards. {3} has {4} cards.\n", roundWinner, player1Name, player1Cards, player2Name, player2Cards);
     }
@@ -47,7 +49,6 @@ static void war(string player1Name, string player2Name) {
     // div. congratulates winner and outputs rounds
     Console.WriteLine("Winner: {0}. Took {1} rounds.", (player1Cards > player2Cards) ? player1Name : player2Name, rounds);
 }
-
 
 /* main, asks for player names before playing War */
 Console.WriteLine("Let's play War!");
