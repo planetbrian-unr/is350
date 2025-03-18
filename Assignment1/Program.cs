ae63044237183373341f31847cc27d3714c13894
Brian Wu <brianwu@unr.edu>
2025-03-18 22:58:28 +0000

    add blank line at end of files

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -7,5 +7,3 @@ Console.WriteLine("Hello world");
 Console.WriteLine("Short-term goal: I want to stay ahead in this course as I feel confident in the material that will be learned.");
 Console.WriteLine("Mid-point goal: I want to understand C# well enough to write simple programs.");
 Console.WriteLine("End-of-semester goal: I want to use C# to reimplement some of my own existing projects.");
-
-
