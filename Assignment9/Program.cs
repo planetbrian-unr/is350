ff68b5e8efe7b4107ef8e8cea25f750e88344e93
Brian Wu <brianwu@unr.edu>
2025-04-15 09:25:02 +0000

    make a9 into a class

--- Assignment9/Program.cs
+++ Assignment9/Program.cs
@@ -4,40 +4,43 @@
     fast food menu manager
 */
 
-// 1
-static string[] defaultMenu() {
-    // returns string array
-    return new string[] { "Cheeseburger", "Chicken Sandwich", "Salad" };
-}
-
-// 2
-static string[] customMenu(int itemCount) {
-    // create string array
-    string[] customMenu = new string[itemCount];
-
-    // loop through and get input
-    for(int i = 0; i < itemCount; i++) {
-        Console.WriteLine("Enter item #{0}:", i + 1);
-        customMenu[i] = Console.ReadLine();
-    }
-
-    // returns array
-    return customMenu;
-}
-
-// 3
-static void printMenu(string[] menu) {
-    // outputs length and contents
-    Console.WriteLine("This menu has {0} items", menu.Length);
-    foreach(string item in menu) {
-        Console.WriteLine(item);
-    }
-}
-
 // 4a
-string[] menu1 = defaultMenu();
-string[] menu2 = customMenu(3);
+string[] menu1 = MenuCreator.DefaultMenu();
+string[] menu2 = MenuCreator.CustomMenu(3);
 
 // 4b
-printMenu(menu1);
-printMenu(menu2);
+MenuCreator.PrintMenu(menu1);
+MenuCreator.PrintMenu(menu2);
+
+public class MenuCreator {
+    // methods
+    // 1
+    public static string[] DefaultMenu() {
+        // returns string array
+        return ["Cheeseburger", "Chicken Sandwich", "Salad"];
+    }
+
+    // 2
+    public static string[] CustomMenu(int itemCount) {
+        // create string array
+        string[] customMenu = new string[itemCount];
+
+        // loop through and get input
+        for(int i = 0; i < itemCount; i++) {
+            Console.WriteLine("Enter item #{0}:", i + 1);
+            customMenu[i] = Console.ReadLine();
+        }
+
+        // returns array
+        return customMenu;
+    }
+
+    // 3
+    public static void PrintMenu(string[] menu) {
+        // outputs length and contents
+        Console.WriteLine("This menu has {0} items", menu.Length);
+        foreach(string item in menu) {
+            Console.WriteLine(item);
+        }
+    }
+}
